4cb67b7ee4e0c32a33186a93f2ba9f39ecfb77ed
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-10-25 21:38:12 -0300

    update

---
+++ hf_download.py
@@ -0,0 +1,24 @@
+import argparse
+from transformers import AutoTokenizer,AutoModelForCausalLM
+import os 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save-path", type=str, default="")
+    parser.add_argument("--model-id", type=str, default="")
+    args = parser.parse_args()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_id)
+    model = AutoModelForCausalLM.from_pretrained(args.model_id)
+
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+    
+    tokenizer.save_pretrained(args.save_path)
+    model.save_pretrained(args.save_path)
+
+
+
+
+if __name__ == "__main__":
+    main()
