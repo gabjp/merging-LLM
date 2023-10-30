a2957d0a69cbc3f1535560a501388834faa32e31
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-10-30 20:36:13 -0300

    update

---
+++ simple_average.py
@@ -0,0 +1,32 @@
+import argparse
+from transformers import AutoTokenizer,AutoModelForCausalLM
+import os 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save-path", type=str, default="")
+    parser.add_argument("--m1", type=str, default="")
+    parser.add_argument("--m2", type=str, default="")
+    parser.add_argument("--p", type=float, defailt=0.5)
+    args = parser.parse_args()
+
+    tokenizer1 = AutoTokenizer.from_pretrained(args.m1, use_fast=False)
+    model1 = AutoModelForCausalLM.from_pretrained(args.m1)
+
+    tokenizer2 = AutoTokenizer.from_pretrained(args.m2, use_fast=False)
+    model2 = AutoModelForCausalLM.from_pretrained(args.m2)
+
+    print(model1.state_dict().keys())
+    print(model2.state_dict().keys())
+
+    #if not os.path.exists(args.save_path):
+    #    os.makedirs(args.save_path)
+    
+    #tokenizer.save_pretrained(args.save_path)
+    #model.save_pretrained(args.save_path)
+
+
+
+
+if __name__ == "__main__":
+    main()
