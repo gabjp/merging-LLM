ec6436d5210390112321d74d2103d851e7dba4e3
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-11-02 18:29:28 -0300

    update

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-import torch
+import json
 
-print(torch.cuda.is_available())
+ques = []
+
+with open("flask_evaluation.jsonl", "r") as jsonl:
+    for line in jsonl:
+        ques_json = json.loads(line)
+        if "Coding" in ques_json["domain_labeled"] or "Culture" in ques_json["domain_labeled"]:
+            ques.append(ques_json)
+
+print(len(ques))
+
+with open("coding_culture.jsonl", "w") as file:
+    for x in ques:
+        json.dump(x, file)
+        file.write("\n")
