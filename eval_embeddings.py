1cc0ca04f02b927ab484af3a9327798314849246
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-11-15 17:55:51 -0300

    update

--- eval_embeddings.py
+++ eval_embeddings.py
@@ -35,7 +35,7 @@ def main():
     v1 = tokenizer1.get_vocab()
     v2 = tokenizer2.get_vocab()
 
-    print(v2[13,])
+    print(v2[13])
     print(v2[829])
     print(v2[29879])
     print(v2[29958])
