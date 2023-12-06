149411ebdbdd7f9c61a0c5ba027ecfa8db7d0cd2
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-12-06 12:54:49 -0300

    update

--- SciMedBenchmark/evaluate.py
+++ SciMedBenchmark/evaluate.py
@@ -56,7 +56,7 @@ def main():
     model.eval()
 
     #Generate Answer
-    q1 = "Hello, what is your name?"
+    q1 = sciq_questions[0]
     inputs = tokenizer(q1, padding=False, return_tensors="pt", truncation=True, max_length=2048).to(device)
     output = model.generate(inputs["input_ids"], do_sample=False, max_new_tokens=64, min_new_tokens=2)
     response = tokenizer.decode(output.tolist()[0], skip_special_tokens=True)
