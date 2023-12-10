28c47a61d261c665afb187f5895ce64b9cf7796a
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-12-10 20:34:27 -0300

    update

--- SciMedBenchmark/evaluate.py
+++ SciMedBenchmark/evaluate.py
@@ -13,33 +13,30 @@ parser.add_argument("--model-name", type=str, default="")
 args = parser.parse_args()
 
 def load_data():
-    f = open('data/pubmed_qa/test_set.json')
+    f = open('/data/pubmed_qa_post/test.json')
     pubmedqa = json.load(f)
-    f = open('data/sciq/test.json')
+    f = open('/data/sciq_post/test.json')
     sciq = json.load(f)
 
     return pubmedqa, sciq
 
-def question_template(question, contexts, options):
-    prompt = f"""Human: {question}\nAnswer '{options[0]}', '{options[1]}', '{options[2]}' or '{options[3]}'.\nAssistant: """
+def question_template(question, options):
+
+    prompt = f"""Human: {question}\n{options}.\nAssistant: """
     return prompt
 
 def main():
     #Load Data
     pubmedqa, sciq = load_data()
 
-    pubmedqa_questions = [pubmedqa[key]["QUESTION"] for key in pubmedqa.keys()]
-    pubmedqa_answer = [pubmedqa[key]["final_decision"] for key in pubmedqa.keys()]
-    pubmedqa_context = [pubmedqa[key]["CONTEXTS"] for key in pubmedqa.keys()]
-    pubmed_options = ['yes', 'no', 'maybe']
+    pubmedqa_questions = [elem["instruction"] for elem in pubmedqa]
+    pubmedqa_answer = [elem["output"] for elem in pubmedqa]
+    pubmedqa_options = [elem["input"] for elem in pubmedqa]
+   
 
-    sciq_questions = [elem["question"] for elem in sciq]
-    sciq_answer = [elem["correct_answer"] for elem in sciq]
-    sciq_context = [elem["support"] for elem in sciq]
-    sciq_options = [random.sample([elem['distractor1'], elem['distractor2'], elem['distractor3'], elem['correct_answer']], 4) for elem in sciq]
-
-    #print(question_template(pubmedqa_questions[0], pubmedqa_context[0], pubmed_options))
-    print(question_template(sciq_questions[0], [sciq_context[0]], sciq_options[0]))
+    sciq_questions = [elem["instruction"] for elem in sciq]
+    sciq_answer = [elem["output"] for elem in sciq]
+    sciq_options = [elem["input"] for elem in sciq]
     
     #Load Model
     tokenizer = AutoTokenizer.from_pretrained(args.model_path)
@@ -47,14 +44,15 @@ def main():
     model.eval()
 
     #Generate Answer
-    q1 = question_template(sciq_questions[0], pubmedqa_context[1], sciq_options[0])
-    inputs = tokenizer(q1, padding=False, return_tensors="pt", truncation=True, max_length=2048).to(device)
-    output = model.generate(inputs["input_ids"], do_sample=False, max_new_tokens=2048, min_new_tokens=2)
-    response = tokenizer.decode(output.tolist()[0], skip_special_tokens=True)
-    print("Response: ")
-    print(response)
-    print(output.tolist())
-    print(inputs)
+    print("evaluating sciq")
+    sciq_prompts = [question_template(question, options) for question, options in zip(sciq_questions, sciq_options)]
+    sciq_lens = [len(question) for question in sciq_prompts]
+    inputs = tokenizer(sciq_prompts, padding=False, return_tensors="pt", truncation=True, max_length=2048).to(device)
+    output = model.generate(inputs["input_ids"], do_sample=False, max_new_tokens=64, min_new_tokens=2)
+    response = tokenizer.decode(output.tolist(), skip_special_tokens=True)
+    preds = [out[i:] for out, i in zip(response, sciq_lens)]
+    print(preds)
+    
 
 
 
