d10b57f2b3249e8e6c58a1361b4a966c70f7a821
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-12-16 15:53:08 -0300

    update

--- SciMedBenchmark/evaluate.py
+++ SciMedBenchmark/evaluate.py
@@ -17,26 +17,30 @@ def load_data():
     pubmedqa = json.load(f)
     f = open('data/sciq_post/test.json')
     sciq = json.load(f)
+    f = open('data/boolq_post/test.json')
+    boolq = json.load(f)
 
-    return pubmedqa, sciq
+    return pubmedqa, sciq, boolq
 
 def question_template(question, options):
-
     prompt = f""" A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.\n Human: {question}\n{options}\nAssistant: """
     return prompt
 
 def main():
     #Load Data
-    pubmedqa, sciq = load_data()
+    pubmedqa, sciq, boolq = load_data()
 
     pubmedqa_questions = [elem["instruction"] for elem in pubmedqa]
     pubmedqa_answer = [elem["output"] for elem in pubmedqa]
     pubmedqa_options = [elem["input"] for elem in pubmedqa]
    
-
     sciq_questions = [elem["instruction"] for elem in sciq]
     sciq_answer = [elem["output"] for elem in sciq]
     sciq_options = [elem["input"] for elem in sciq]
+
+    boolq_questions = [elem["instruction"] for elem in boolq]
+    boolq_answer = [elem["output"] for elem in boolq]
+    boolq_options = [elem["input"] for elem in boolq]
     
     #Load Model
     tokenizer = AutoTokenizer.from_pretrained(args.model_path)
@@ -58,21 +62,6 @@ def main():
     preds = [out[i:] for out, i in zip(response, sciq_lens)]
     print(preds)
 
-    sciq_count = [int(true in pred) for true,pred in zip(sciq_answer, preds)]
-    print(f"Sciq acc (contained): {sum(sciq_count)/len(sciq_count)}")
-
-    sciq_count = [int(true == pred) for true,pred in zip(sciq_answer, preds)]
-    print(f"Sciq acc (equal): {sum(sciq_count)/len(sciq_count)}")
-
-    sciq_count = [int(true.replace(" ", "") == pred.replace(" ", "")) for true,pred in zip(sciq_answer, preds)]
-    print(f"Sciq acc (equal removing space): {sum(sciq_count)/len(sciq_count)}")
-    
-    sciq_count = [int(true in pred.partition('\n')[0]) for true,pred in zip(sciq_answer, preds)]
-    print(f"Sciq acc (contained first line): {sum(sciq_count)/len(sciq_count)}")
-
-    sciq_count = [int(true == pred.partition('\n')[0]) for true,pred in zip(sciq_answer, preds)]
-    print(f"Sciq acc (equal first line): {sum(sciq_count)/len(sciq_count)}")
-
     sciq_count = [int((true in pred.partition('\n')[0]) and (len(pred.partition('\n')[0]) <= 2.5*len(true))) for true,pred in zip(sciq_answer, preds)]
     print(f"Sciq acc (contained first line and length): {sum(sciq_count)/len(sciq_count)}")
     
@@ -90,27 +79,27 @@ def main():
     preds = [out[i:] for out, i in zip(response, pubmedqa_lens)]
     print(preds)
 
-    pubmedqa_count = [int(true in pred) for true,pred in zip(pubmedqa_answer, preds)]
-    print(f"pubmedqa acc (contained): {sum(pubmedqa_count)/len(pubmedqa_count)}")
-
-    pubmedqa_count = [int(true == pred) for true,pred in zip(pubmedqa_answer, preds)]
-    print(f"pubmedqa acc (equal): {sum(pubmedqa_count)/len(pubmedqa_count)}")
-
-    pubmedqa_count = [int(true.replace(" ", "") == pred.replace(" ", "")) for true,pred in zip(pubmedqa_answer, preds)]
-    print(f"pubmedqa acc (equal removing space): {sum(pubmedqa_count)/len(pubmedqa_count)}")
-    
-    pubmedqa_count = [int(true in pred.partition('\n')[0]) for true,pred in zip(pubmedqa_answer, preds)]
-    print(f"pubmedqa acc (contained first line): {sum(pubmedqa_count)/len(pubmedqa_count)}")
-
-    pubmedqa_count = [int(true == pred.partition('\n')[0]) for true,pred in zip(pubmedqa_answer, preds)]
-    print(f"pubmedqa acc (equal first line): {sum(pubmedqa_count)/len(pubmedqa_count)}")
-
-    pubmedqa_count = [int((true in pred.partition('\n')[0]) and (len(pred.partition('\n')[0]) <= 2.5*len(true))) for true,pred in zip(pubmedqa_answer, preds)]
-    print(f"pubmedqa acc (contained first line and length): {sum(pubmedqa_count)/len(pubmedqa_count)}")
-
     pubmedqa_count = [int((true in pred.split()[0])) for true,pred in zip(pubmedqa_answer, preds)]
     print([pred.split()[0] for pred in preds])
     print(f"pubmedqa acc (contained first word): {sum(pubmedqa_count)/len(pubmedqa_count)}")
+
+    print("evaluating boolq")
+    response = []
+    boolq_prompts = [question_template(question, options) for question, options in zip(boolq_questions, boolq_options)]
+    boolq_lens = [len(question) for question in boolq_prompts]
+    for idx in range(0, len(boolq_prompts)):
+        inputs = tokenizer(boolq_prompts[idx], padding=False, return_tensors="pt", truncation=True, max_length=2048).to(device)
+        output = model.generate(**inputs, do_sample=False, max_new_tokens=64, min_new_tokens=2)
+        for out in output.tolist():
+            decoded = tokenizer.decode(out, skip_special_tokens=True)
+            #print(decoded)
+            response.append(decoded)
+    preds = [out[i:] for out, i in zip(response, boolq_lens)]
+    print(preds)
+
+    boolq_count = [int((true in pred.split()[0])) for true,pred in zip(boolq_answer, preds)]
+    print([pred.split()[0] for pred in preds])
+    print(f"boolq acc (contained first word): {sum(boolq_count)/len(boolq_count)}")
     
 
 if __name__ == "__main__":
