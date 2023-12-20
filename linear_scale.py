d9ec88409a6589368ff06bf74b4b5e7d6f844a77
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-12-20 02:39:19 -0300

    update

--- linear_scale.py
+++ linear_scale.py
@@ -9,7 +9,7 @@ import numpy as np
 def layer(string_list):
     if len(string_list) >=3 and string_list[2] == 'weight' and string_list[1] != 'norm':
         idx = 0
-    elif len(string_list) >=3:
+    elif len(string_list) >=3 and string_list[2] != 'weight':
         idx = int(string_list[2]) + 1
     else:
         idx = 33
