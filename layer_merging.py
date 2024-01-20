475ccf7285cf790d78ea70e1f01c7d787aab23c2
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-01-20 16:42:43 -0300

    update

--- layer_merging.py
+++ layer_merging.py
@@ -8,48 +8,66 @@ from peft import PeftModel
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model-path", type=str, default="")
-    parser.add_argument("--v", type=int, default=0)
+    parser.add_argument("--m1", type=str, default="")
+    parser.add_argument("--m2", type=str, default="")
     parser.add_argument("--llama-path", type=str, default="meta-llama/Llama-2-7b-hf")
+    parser.add_argument("--save-path", type=str, default="")
     args = parser.parse_args()
 
+    tokenizer = AutoTokenizer.from_pretrained(args.llama_path, use_fast=False)
 
-    llama = AutoModelForCausalLM.from_pretrained(args.llama_path)
+    llama1 = AutoModelForCausalLM.from_pretrained(args.llama_path)
+    llama2 = AutoModelForCausalLM.from_pretrained(args.llama_path)
     llama_base = AutoModelForCausalLM.from_pretrained(args.llama_path)
 
-    model = PeftModel.from_pretrained(llama_base, args.model_path)
+    model1_unmerged = PeftModel.from_pretrained(llama_base, args.m1)
+    model1 = PeftModel.from_pretrained(llama1, args.m1)
+    model2 = PeftModel.from_pretrained(llama2, args.m2)
 
-    model.merge_adapter()
+    model1.merge_adapter()
+    model2.merge_adapter()
 
-    sd_llama = llama.named_parameters()
-    sd_model = model.named_parameters()
+    sd_model1_unmerged = model1_unmerged.named_parameters()
+    sd_model1 = model1.named_parameters()
+    sd_model2 = model2.named_parameters()
 
-    print("MERGED")
-    for (name, val) in sd_model:
-        print(name)
-        if 'transformer.h.0.self_attention.query_key_value.weight' in name:
-            print(val)
-            print(name)
-            break
-
-    print()
-    print("BASE")
-    for (name, val) in sd_llama:
-        print(name)
-        if 'transformer.h.0.self_attention.query_key_value.weight' in name:
-            print(val)
-            print(name)
-            break
-
-    #for ((name1,val_llama),(name2,val_model)) in zip(sd_llama,sd_model):
+    it = zip(sd_model1_unmerged,sd_model1, sd_model2)
 
 
-        # val_model.sub_(val_llama)
-        # if torch.sum(torch.abs(val_model)) != 0 or args.v == 1:
-        #     print(name1)
-        #     print(f"Sum: {torch.sum(torch.abs(val_model))}")
-        #     print(f"Mean: {torch.mean(torch.abs(val_model))}")
-        #     print()
+    for ((n,v1),(_,v2), (_,v3)) in it:
+        if 'query_key_value.weight' in n or 'dense_h_to_4h.weight' in n or 'dense_4h_to_h.weight' in n:
+            # COMPARE DELTA AND MERGE NEXT 2 MATRICES (LORA ADAPTERS)
+
+            delta_1 = torch.sum(torch.abs(v2-v1))
+            delta_2 = torch.sum(torch.abs(v3-v1))
+
+            print(n)
+            print(f"delta 1 {delta_1}")
+            print(f"delta 2 {delta_2}")
+
+            if delta_1 >= delta_2:
+                p = 0.9
+            else:
+                p = 0.1
+
+            ((n,v1),(_,v2), (_,v3)) = next(it)
+            print(f"merging {n} p={p}")
+            v1.mul_(p)
+            v1.add_(v3 * p)
+
+            ((n,v1),(_,v2), (_,v3)) = next(it)
+            print(f"merging {n} p={p}")
+            v1.mul_(p)
+            v1.add_(v3 * p)
+
+        print("merged")
+
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+
+    tokenizer.save_pretrained(args.save_path)
+    model1_unmerged.save_pretrained(args.save_path)
+
 
 
 if __name__ == "__main__":
