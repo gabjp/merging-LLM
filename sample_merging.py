aad75c3560c1a18c375ace8ede2e8af0a1bdf36e
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-01-30 21:09:01 -0300

    update

---
+++ sample_merging.py
@@ -0,0 +1,50 @@
+import argparse
+from transformers import AutoTokenizer,AutoModelForCausalLM
+import os 
+import torch
+import random
+import gc
+from peft import PeftModel 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save-path", type=str, default="")
+    parser.add_argument("--m1", type=str, default="")
+    parser.add_argument("--m2", type=str, default="")
+    parser.add_argument("--llama-path", type=str, default="meta-llama/Llama-2-7b-hf")
+    parser.add_argument("--p", type=float, default=0.5)
+    args = parser.parse_args()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.llama_path, use_fast=False)
+    llama1 = AutoModelForCausalLM.from_pretrained(args.llama_path)
+    llama2 = AutoModelForCausalLM.from_pretrained(args.llama_path)
+
+    model1 = PeftModel.from_pretrained(llama1, args.m1).merge_and_unload()
+    model2 = PeftModel.from_pretrained(llama2, args.m2).merge_and_unload()
+
+    sd1 = model1.named_parameters()
+    sd2 = model2.named_parameters()
+
+    for ((name1,val1),(name2,val2)) in zip(sd1,sd2):
+        print(f"merging {name1}")
+        mask = torch.rand(val1.size()) < args.p
+        val1.mul_(mask)
+        val1.add_((~mask) * val2)
+    
+
+
+    print("merged")
+
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+
+    
+    tokenizer.save_pretrained(args.save_path)
+    model1.save_pretrained(args.save_path)
+
+
+
+
+if __name__ == "__main__":
+    with torch.no_grad():
+        main()
