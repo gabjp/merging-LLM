9184041f6b7e8b5d101791fdc06d866e1a6adc8d
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-02-29 15:35:35 -0300

    update

--- apply_lora_average.py
+++ apply_lora_average.py
@@ -11,11 +11,10 @@ def main():
     parser.add_argument("--save-path", type=str, default="")
     parser.add_argument("--m1", type=str, default="")
     parser.add_argument("--m2", type=str, default="")
+    parser.add_argument("--m3", type=str, default="")
     parser.add_argument("--llama-path", type=str, default="meta-llama/Llama-2-7b-hf")
     parser.add_argument("--p", type=float, default=0.5)
     args = parser.parse_args()
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     tokenizer = AutoTokenizer.from_pretrained(args.llama_path, use_fast=False)
     llama1 = AutoModelForCausalLM.from_pretrained(args.llama_path)
@@ -27,13 +26,23 @@ def main():
     sd1 = model1.named_parameters()
     sd2 = model2.named_parameters()
 
-    for ((name1,val1),(name2,val2)) in zip(sd1,sd2):
-        print(f"merging {name1}")
-        print(val1)
-        print(val2)
-        val1.mul_(args.p)
-        val2.mul_(1-args.p)
-        val1.add_(val2)
+    if args.m3 != "":
+        llama3 = AutoModelForCausalLM.from_pretrained(args.llama_path)
+        model3 = PeftModel.from_pretrained(llama3, args.m3).merge_and_unload()
+        sd3 = model3.named_parametes()
+
+        for ((name1,val1),(name2,val2), (name3,val3)) in zip(sd1, sd2, sd3):
+            print(f"merging {name1}")
+            val1.copy_(torch.stack([val1, val2, val3], dim=0).mean(dim=0))
+
+    else:
+        for ((name1,val1),(name2,val2)) in zip(sd1,sd2):
+            print(f"merging {name1}")
+            print(val1)
+            print(val2)
+            val1.mul_(args.p)
+            val2.mul_(1-args.p)
+            val1.add_(val2)
     
 
 
