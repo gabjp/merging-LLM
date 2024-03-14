eaa5142ee4c5b378596bedd8212d677529102827
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-03-13 23:12:03 -0300

    update

--- merge_lora_save.py
+++ merge_lora_save.py
@@ -13,20 +13,16 @@ def main():
     parser.add_argument("--save-path", type=str, default="")
     args = parser.parse_args()
 
-    print("hi")
-
     tokenizer = AutoTokenizer.from_pretrained(args.llama_path, use_fast=False)
     base = AutoModelForCausalLM.from_pretrained(args.llama_path)
     model = PeftModel.from_pretrained(base, args.m).merge_and_unload()
 
-    print("hi")
-
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path)
-    
-    print("hi")
+
 
     tokenizer.save_pretrained(args.save_path)
     model.save_pretrained(args.save_path)
 
-    print("hi")
+if __name__ == "__main__":
+    main()
